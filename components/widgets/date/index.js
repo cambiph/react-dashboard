a160a7ae1a1955841903b5191389366e140ed70a
Philippe Cambien <philippe.cambien@gmail.com>
2018-06-05 15:22:27 +0200

    Refactor

--- components/widgets/date/index.js
+++ components/widgets/date/index.js
@@ -1,29 +1,29 @@
-import {Component} from 'react'
+import { Component } from 'react'
 import Widget from '../../widget'
 import Clock from 'react-live-clock'
 import Weather from '../weather'
 
 export default class Date extends Component {
-    render() {
-        return <Widget>
-            <div style={{fontSize: '4em', textAlign: 'center'}}>
-                <Clock
-                    format={'HH:mm:ss'}
-                    ticking={true}
-                    timezone={'Europe/Brussels'}/>
-            </div>
-            <div style={{fontSize: '1em', textAlign: 'center'}}>
-                <Clock
-                    format={'ddd, d MMMM YYYY'}
-                    timezone={'Europe/Brussels'}/>
-            </div>
-            <div style={{fontSize: '2em', textAlign: 'center'}}>
-              <Weather
-                city='Mechelen'
-                units='metric'
-                apiKey='c3a848060e0570d311a3b93c4d6a6eb1'
-              />
-            </div>
-        </Widget>
-    }
+  render () {
+    return <Widget>
+      <div style={{fontSize: '4em', textAlign: 'center'}}>
+        <Clock
+          format={'HH:mm:ss'}
+          ticking={true}
+          timezone={'Europe/Brussels'}/>
+      </div>
+      <div style={{fontSize: '2em', textAlign: 'center'}}>
+        <Clock
+          format={'ddd, d MMMM YYYY'}
+          timezone={'Europe/Brussels'}/>
+      </div>
+      <div style={{fontSize: '2em', textAlign: 'center'}}>
+        <Weather
+          city='Mechelen'
+          units='metric'
+          apiKey='c3a848060e0570d311a3b93c4d6a6eb1'
+        />
+      </div>
+    </Widget>
+  }
 }
--- components/widgets/weather/index.js
+++ components/widgets/weather/index.js
@@ -1,52 +1,53 @@
 import { Component } from 'react'
 import fetch from 'isomorphic-unfetch'
 import yup from 'yup'
-import Temperature from '../../temperature'
 
 const schema = yup.object().shape({
-    city: yup.string().required(),
-    units: yup.string(),
-    apiKey: yup.string()
+  city: yup.string().required(),
+  units: yup.string().required(),
+  apiKey: yup.string().required()
 })
 
 export default class Weather extends Component {
-    static defaultProps = {
-        interval: 1000 * 60 * 5
+  static defaultProps = {
+    interval: 1000 * 60 * 5
+  }
+
+  state = {
+    temp: 0
+  }
+
+  componentDidMount () {
+    schema.validate(this.props)
+      .then(() => this.fetchInformation())
+      .catch((err) => {
+        console.error(`${err.name} @ ${this.constructor.name}`, err.errors)
+      })
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.timeout)
+  }
+
+  async fetchInformation () {
+    const {city, units, apiKey} = this.props
+
+    try {
+      const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`)
+      const json = await res.json()
+
+      this.setState({temp: json.main.temp})
+    } finally {
+      this.timeout = setTimeout(() => this.fetchInformation(), this.props.interval)
     }
+  }
 
-    state = {
-        temp: 0
-    }
-
-    componentDidMount() {
-        schema.validate(this.props)
-            .then(() => this.fetchInformation())
-            .catch((err) => {
-                console.error(`${err.name} @ ${this.constructor.name}`, err.errors)
-            })
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this.timeout)
-    }
-
-    async fetchInformation() {
-        const {city, units, apiKey} = this.props
-
-        try {
-            const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`)
-            const json = await res.json()
-
-            this.setState({temp: json.main.temp})
-        } finally {
-            this.timeout = setTimeout(() => this.fetchInformation(), this.props.interval)
-        }
-    }
-
-    render() {
-        const {temp} = this.state
-        return (
-                <Temperature value={Math.round(temp)}/>
-        )
-    }
+  render () {
+    const {temp} = this.state
+    return (
+      <div>
+        <span>{Math.round(temp)}°C</span>
+      </div>
+    )
+  }
 }
